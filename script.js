383e9bc8139665f7b981a7f5fab5a04e461b528d
João Oliveira <jaoovit@gmail.com>
2024-03-27 12:00:36 +0000

    add a 3 functions to initialize the game, to pass information for the array clicking and update the cell status

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-const Gameboard = (function gameboard() {
-    const board = ['', '', '',
-                   '', '', '',
-                   '', '', '']
-    return {board};
-    
-})();
-
-console.log
-
-// Factory to create players
-
-function createPlayer (name, mark) {
-    const playerName = name;
-    const playerMark = mark;
-    return{playerName, playerMark}
-}
-
-const playerOne = createPlayer('', 'x')
-const playerTwo = createPlayer('', 'o')
+const cells = document.querySelectorAll('.cell');
+const statusText = document.querySelector('.gameStatus');
+const restartBtn = document.querySelector('.restartBtn');
 
 // Check winner
 
@@ -30,6 +13,63 @@ const winningCombinations = [
     [2, 5, 8],
     [0, 4, 8],
     [6, 4, 2]
-]
+];
+
+const Gameboard = (function gameboard() {
+    let board = ['', '', '',
+                   '', '', '',
+                   '', '', '']
+    return {board};
+})();
+
+console.log(Gameboard.board)
+
+// Factory to create players
+
+function createPlayer (name, mark) {
+    const playerName = name;
+    const playerMark = mark;
+    return{playerName, playerMark}
+};
+
+let playerOne = createPlayer('', 'x');
+let playerTwo = createPlayer('', 'o');
+
+let currentPlayer = playerOne.playerMark;
+let running = false;
+
+initializeGame();
+
+function initializeGame() {
+    cells.forEach(cell => cell.addEventListener('click', cellClicked))
+    restartBtn.addEventListener('click', restartGame)
+    statusText.textContent = `${currentPlayer}'s turn`
+    running = true;
+};
+
+function cellClicked() {
+    const cellIndex = this.getAttribute('cellIndex');
+
+    if(Gameboard.board[cellIndex] != "" || !running) {
+        return;
+    }
+
+    updateCell(this, cellIndex);
+    checkWinner()
+}
+
+function restartGame() {
+
+}
+
+function updateCell(cell, index) {
+    Gameboard.board[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+    console.log(Gameboard.board)
+}
+
+function checkWinner() {
+
+}
 
 
